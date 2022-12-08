820c5593c52155edb5182b07c3ad7f6d14ea09fb
minkyoj <minkyo0816@gmail.com>
2022-12-09 00:46:10 +0900

    12.09

--- Final_workshop/Final_Workshop_Worksheet.sql
+++ Final_workshop/Final_Workshop_Worksheet.sql
@@ -29,11 +29,61 @@ JOIN TB_WRITER USING (WRITER_NO)
 WHERE COMPOSE_TYPE = '옮김';
 
 -- 6. 300권 이상 등록된 도서의 저작 형태 및 등록된 도서 수량을 표시하는 SQL 구문을 작성하시오.
--- (저작형태가 등록되지 않은 경우는 제외할 것)
+-- (저작형태가 등록되지 않은 경우는 제외할 것) // COMPOSE_TYPE , 
+
+SELECT * FROM TB_BOOK;
+SELECT * FROM TB_BOOK_AUTHOR;
 
 
+SELECT COMPOSE_TYPE, COUNT(*)
+FROM TB_BOOK
+JOIN TB_BOOK_AUTHOR USING (BOOK_NO)
+WHERE PAGE >= 300;
+
+-- 7. 가장 최근에 발간된 최신작 이름과 발행일자, 출판사 이름을 표시하는 SQL 구문을 작성하시오
+
+SELECT BOOK_NM, ISSUE_DATE, PUBLISHER_NM
+FROM TB_BOOK
+ORDER BY ISSUE_DATE DESC;
 
 
+SELECT *
+FROM (SELECT BOOK_NM, ISSUE_DATE, PUBLISHER_NM
+        FROM TB_BOOK
+        ORDER BY ISSUE_DATE DESC)
+WHERE ROWNUM = 1;
+
+-- 8. 가장 많은 책을 쓴 작가 3명의 이름과 수량을 표시하되, 많이 쓴 순서대로 표시하는 SQL 구문을 작성하시오.
+-- 단, 동명이인(同名異人) 작가는 없다고 가정한다. (결과 헤더는 “작가 이름”, “권 수”로 표시되도록 할
+-- 것)
+
+/*
+SELECT * FROM TB_BOOK_AUTHOR;
+SELECT * FROM TB_BOOK;
+SELECT * FROM TB_WRITER;
+
+SELECT WRITER_NM AS "작가 이름", COUNT (*) AS "권수"
+FROM TB_BOOK_AUTHOR
+JOIN TB_WRITER USING (WRITER_NO)
+GROUP BY WRITER_NM
+ORDER BY COUNT(*) DESC;
+*/
+
+SELECT *
+FROM (SELECT WRITER_NM AS "작가 이름", COUNT (*) AS "권수"
+        FROM TB_BOOK_AUTHOR
+        JOIN TB_WRITER USING (WRITER_NO)
+        GROUP BY WRITER_NM
+        ORDER BY COUNT(*) DESC)
+WHERE ROWNUM <= 3;
+
+-- 9. 작가 정보 테이블의 모든 등록일자 항목이 누락되어 있는 걸 발견하였다. 누락된 등록일자 값을 각 작가의
+-- ‘최초 출판도서의 발행일과 동일한 날짜’로 변경시키는 SQL 구문을 작성하시오. (COMMIT 처리할 것)
+
+SELECT * FROM TB_WRITER;
+SELECT * FROM TB_BOOK; 
+SELECT * FROM TB_PUBLISHER;
+SELECT * FROM TB_BOOK_AUTHOR; --
 
 
 
